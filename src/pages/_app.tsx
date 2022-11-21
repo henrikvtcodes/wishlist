5f28fabdaf25cbf6f13fa47daddf5541aff02a91
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-21 16:18:48 -0500

    add inter local font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,31 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Inter } from "@next/font/google";
 
 import { trpc } from "../utils/trpc";
+import { UserWatcher } from "components/UserWatcher";
 
 import "../styles/globals.css";
-import { UserWatcher } from "components/UserWatcher";
+
+const inter = Inter();
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <UserWatcher />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <UserWatcher />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
