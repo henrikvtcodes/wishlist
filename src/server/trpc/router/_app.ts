00a8840b4a4e02f9ce009e2d8309634d64697d72
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-21 09:21:26 -0500

    remove example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { refsRouter } from "./refs";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   refs: refsRouter,
 });
