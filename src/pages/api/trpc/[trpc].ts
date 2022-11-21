1a3425d5fbd2b6aa612c8f796f56e587bcec683d
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-20 22:48:39 -0500

    init t3

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,13 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { createContext } from "server/trpc/context";
+import { appRouter } from "server/trpc/router/_app";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError: ({ path, error }) => {
+    console.error(`❌ tRPC failed on ${path}: ${error}`);
+  },
+});
