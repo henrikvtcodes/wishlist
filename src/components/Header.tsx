5de2c4bd39133b5ca76013f0da2b091a62f02d67
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-21 22:29:32 -0500

    better header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,140 @@
 import Image from "next/image";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
+
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const HeaderBarLink = ({ title, href }: { title: string; href: string }) => {
+  const router = useRouter();
+  const category = router.query.category as string;
+
+  return (
+    <NextLink
+      href={href}
+      className={classNames(
+        "text-base font-medium",
+        href.includes(category)
+          ? "text-blue-500 "
+          : "text-gray-500 hover:text-gray-900"
+      )}
+    >
+      {title}
+    </NextLink>
+  );
+};
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 flex w-screen items-center gap-x-1 border-b-2 border-blue-500 bg-blue-200 px-2 py-3">
-      <span className="flex-1 first:mr-auto">
-        <h1 className="text-lg font-bold text-cyan-900 first:mr-auto md:text-2xl">
-          {" "}
-          Henrik&apos;s Wishlist
-        </h1>
-      </span>
-      <nav className="flex justify-evenly gap-x-2 font-medium">
-        <NextLink
-          href={`/tools`}
-          className={`rounded bg-blue-300 p-0 hover:bg-blue-400 sm:px-2 sm:py-1`}
-        >
-          {" "}
-          Tools{" "}
-        </NextLink>
-        <NextLink
-          href={`/tech`}
-          className={`rounded bg-blue-300 p-0 hover:bg-blue-400 sm:px-2 sm:py-1`}
-        >
-          {" "}
-          Electronics{" "}
-        </NextLink>
-        <NextLink
-          href={`/legos`}
-          className={`rounded bg-blue-300 p-0 hover:bg-blue-400 sm:px-2 sm:py-1`}
-        >
-          {" "}
-          Lego{" "}
-        </NextLink>
-        <NextLink
-          href={`/clothing`}
-          className={`rounded bg-blue-300 p-0 hover:bg-blue-400 sm:px-3 sm:py-2`}
-        >
-          {" "}
-          Clothing{" "}
-        </NextLink>
-      </nav>
+    <Popover className="relative bg-white">
+      <div
+        className="pointer-events-none absolute inset-0 z-30 shadow"
+        aria-hidden="true"
+      />
+      <div className="relative z-20">
+        <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-5 sm:px-6 sm:py-4 md:justify-start md:space-x-10 lg:px-8">
+          <div>
+            <NextLink href="/" className="flex">
+              <Image
+                src={"/undergroundhenrik-round.png"}
+                width={56}
+                height={56}
+                alt="Underground Henrik Logo"
+              />
+            </NextLink>
+          </div>
+          <div className="-my-2 -mr-2 md:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <div className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+            <nav className="flex space-x-10">
+              <HeaderBarLink title="Techology" href="/tech" />
+              <HeaderBarLink title="Tools" href="/tools" />
+              <HeaderBarLink title="Lego" href="/legos" />
+              <HeaderBarLink title="Clothing" href="/clothing" />
+            </nav>
+            <div className="flex items-center md:ml-12">
+              <span className="text-base font-medium text-gray-500 hover:text-gray-900">
+                User not found
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <span className="flex-1 text-right last:ml-auto"> No User</span>
-    </header>
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden"
+        >
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6 sm:pb-8">
+              <div className="flex items-center justify-between">
+                <div>
+                  <NextLink href="#" className="flex">
+                    <Image
+                      src={"/undergroundhenrik-round.png"}
+                      width={56}
+                      height={56}
+                      alt="Underground Henrik Logo"
+                    />
+                  </NextLink>
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+            <div className="py-6 px-5">
+              <div className="grid grid-cols-2 gap-4">
+                <NextLink
+                  href="/tech"
+                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Technology
+                </NextLink>
+                <NextLink
+                  href="/tools"
+                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Tools
+                </NextLink>
+                <NextLink
+                  href="/legos"
+                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Legos
+                </NextLink>
+                <NextLink
+                  href="/clothing"
+                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Clothing
+                </NextLink>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 };
