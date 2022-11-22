07bb312c71077161c53f895c3cc5705738a2c58d
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-22 09:54:25 -0500

    fix horizontal scrolling bug

--- src/layouts/Category.tsx
+++ src/layouts/Category.tsx
@@ -3,9 +3,9 @@ import { Header } from "components/Header";
 
 export const CategoryLayout: FC<{ children?: ReactNode }> = ({ children }) => {
   return (
-    <div className="min-h-screen w-screen bg-gray-100">
+    <div className="min-h-screen w-screen max-w-full bg-gray-100">
       <Header />
-      <main className="flex w-screen flex-col items-center justify-start divide-y-2 divide-gray-300">
+      <main className="flex w-full flex-col items-center justify-start divide-y-2 divide-gray-300 overflow-x-clip">
         {children}
       </main>
     </div>
