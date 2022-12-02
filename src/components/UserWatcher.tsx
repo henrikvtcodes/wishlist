9e677b9bfbf4a8c0b54dcaa11fd3fdbda18b9e01
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-12-02 13:07:00 -0500

    plausible event

--- src/components/UserWatcher.tsx
+++ src/components/UserWatcher.tsx
@@ -1,3 +1,4 @@
+import { usePlausible } from "next-plausible";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
@@ -6,7 +7,9 @@ import { trpc } from "utils/trpc";
 
 export const UserWatcher = () => {
   const router = useRouter();
+  const plausible = usePlausible();
   const setUser = useStoredUser((state) => state.setUser);
+  const user = useStoredUser((state) => state.user);
 
   const { data } = trpc.refs.getRef.useQuery({
     ref: String(router.query.userRef),
@@ -17,6 +20,12 @@ export const UserWatcher = () => {
       setUser({ name: data.name, id: data.id });
     }
   }, [data, setUser]);
+
+  useEffect(() => {
+    if (user !== null) {
+      plausible("RefView", { props: { ref: user.name } });
+    }
+  }, [user, plausible]);
 
   return <></>;
 };
