f384d5e9a21f3b060914fbdf1cc7376e87bedde8
henrikvtcodes <commits@henrikvt.com>
2023-10-22 21:04:33 -0400

    get drizzle workin

--- new/drizzle.config.ts
+++ new/drizzle.config.ts
@@ -4,9 +4,10 @@ import { env } from "~/env.mjs";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  driver: "mysql2",
+  out: "./drizzle",
+  driver: "pg",
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
-  tablesFilter: ["new_*"],
+  tablesFilter: ["*"],
 } satisfies Config;
--- new/src/server/db/index.ts
+++ new/src/server/db/index.ts
@@ -1,12 +1,7 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema }
-);
+export const db = drizzle(postgres(env.DATABASE_URL), { schema });
