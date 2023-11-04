d8b79bc7e9494cf0a4569f089fda7f980bb15dd9
henrikvtcodes <commits@henrikvt.com>
2023-11-04 13:07:30 -0400

    use actual timestamps

---
+++ drizzle/0001_glamorous_archangel.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "wishlist_Item" ALTER COLUMN "createdAt" SET DATA TYPE timestamp (3);--> statement-breakpoint
+ALTER TABLE "wishlist_Item" ALTER COLUMN "claimedAt" SET DATA TYPE timestamp (3);
