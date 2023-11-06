166e8255ea2595899304299855a8dabbfbb095d5
henrikvtcodes <commits@henrikvt.com>
2023-11-06 12:40:59 -0500

    fix hydration error in header

--- src/app/(public)/header-bar-user.tsx
+++ src/app/(public)/header-bar-user.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useIsClient } from "usehooks-ts";
+
 import { useStoredUser } from "~/stores/storedUser";
 
 export function HeaderBarUser() {
+  const isClient = useIsClient();
   const userName = useStoredUser((state) => state.user?.name);
 
+  if (!isClient) return null;
+
   return userName ? (
-    <span className="text-base font-medium text-gray-600 inline-flex">
+    <span className="inline-flex text-base font-medium text-gray-600">
       {"Hi"}
-      <span className="font-semibold pl-1">{userName}</span>
+      <span className="pl-1 font-semibold">{userName}</span>
     </span>
   ) : null;
 }
