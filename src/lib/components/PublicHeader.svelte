9228cf9a44bdf28a4706839db8a0577e5c012c9b
Henrik VT <commits@henrikvt.com>
2025-11-21 13:46:08 -0500

    add header dropdown for small screens

--- src/lib/components/PublicHeader.svelte
+++ src/lib/components/PublicHeader.svelte
@@ -5,6 +5,8 @@
 	import { useQuery } from 'convex-svelte';
 	import { api } from '$convex/_generated/api';
 	import { env } from '$lib/env/client';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { buttonVariants } from './ui/button';
 
 	const categories = useQuery(api.categories.all, {}, () => ({}));
 </script>
@@ -12,7 +14,30 @@
 <header class="flex w-screen justify-between p-2">
 	<Card class="w-full flex-row p-4"
 		><a href="/" class="flex items-center"><TreePineIcon class="h-auto w-8" /></a>
-		<div class="links flex grow basis-full items-center gap-x-2 *:h-min *:underline">
+		<div class="flex grow basis-full items-center md:hidden">
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline' })}
+					>Categories</DropdownMenu.Trigger
+				>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						{#if categories.data !== null && categories.data !== undefined}
+							{#each categories.data as ct}
+								<DropdownMenu.Item>
+									<a class="underline" href="/{ct.slug}">{ct.name}</a>
+								</DropdownMenu.Item>
+							{/each}
+						{/if}
+						<DropdownMenu.Separator />
+
+						<DropdownMenu.Item>
+							<a class="underline" href="/donate">Donate</a>
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</div>
+		<div class="hidden grow basis-full items-center gap-x-2 *:h-min *:underline md:flex">
 			{#if categories.data !== null && categories.data !== undefined}
 				{#each categories.data as ct}
 					<a href="/{ct.slug}">{ct.name}</a>
